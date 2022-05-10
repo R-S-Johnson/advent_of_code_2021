d40864f122ae8fa2b47928f393735afbb9ae1c16
Riley Johnson <riley.scott.johnson@gmail.com>
2022-05-10 18:27:53 -0400

    day 1 finished

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,14 +1,16 @@
 
 
 f = open("day_1/input.txt", "r")
+lines = f.readlines()
 
-next = int(f.readline().strip())
-previous = None
+next = 1
+current = 0
 count = 0
 
-for line in f.readlines():
-    previous = next
-    next = int(line.strip())
-    if previous < next:
+for i in range(len(lines) - 3):
+    if int(lines[current].strip()) + int(lines[current + 1].strip()) + int(lines[current + 2].strip()) < int(lines[next].strip()) + int(lines[next + 1].strip()) + int(lines[next + 2].strip()):
         count += 1
+    next += 1
+    current += 1
+
 print(count)
