8e9656fd6a39717076492ce5c5d9bdfa940defd7
Winter Johnson <riley.scott.johnson@gmail.com>
2023-12-07 08:43:16 -0500

    day 1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,16 +1,17 @@
 
 
-f = open("day_1/input.txt", "r")
-lines = f.readlines()
+with open("day_1/input.txt", "r") as f:
+    data = f.readlines()
+    data = [int(i) for i in data]
 
-next = 1
-current = 0
 count = 0
 
-for i in range(len(lines) - 3):
-    if int(lines[current].strip()) + int(lines[current + 1].strip()) + int(lines[current + 2].strip()) < int(lines[next].strip()) + int(lines[next + 1].strip()) + int(lines[next + 2].strip()):
+summ = [sum(data[(0+i):(3+i)]) for i in range(len(data)-2)]
+prev = summ[0]
+
+for elem in summ:
+    if elem > prev:
         count += 1
-    next += 1
-    current += 1
+    prev = elem
 
 print(count)
